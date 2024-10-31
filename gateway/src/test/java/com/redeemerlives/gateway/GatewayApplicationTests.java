abba5d6833220beac94db0373330dcaabad48a66
GideonAg <gideonagbosu72@gmail.com>
2024-10-31 13:12:34 +0000

    Add gateway

---
+++ gateway/src/test/java/com/redeemerlives/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redeemerlives.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
