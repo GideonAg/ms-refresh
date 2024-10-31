6edd1c1da51ddba6bb37987c195763e8a92361df
GideonAg <gideonagbosu72@gmail.com>
2024-10-31 13:11:17 +0000

    Add config server

---
+++ config-server/src/test/java/com/redeemerlives/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redeemerlives.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
